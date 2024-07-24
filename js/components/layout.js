8e2e3fd102f0809d4152ed70e993c95d5ba908e0
tomasdargis <167984812+tomasdargis@users.noreply.github.com>
2024-07-24 23:16:41 +0300

    initial Layout rendering

---
+++ js/components/layout.js
@@ -0,0 +1,29 @@
+export class layout {
+    constructor() {
+
+    }
+    header() {
+        const HTML = '<header>HEADER</header>';
+        return HTML;
+    }
+    main() {
+        const HTML = '<main>MAIN</main>';
+        return HTML;
+
+
+    }
+    footer() {
+        const HTML = '<footer>FOOTER</footer>';
+        return HTML;
+
+
+
+    }
+    render() {
+        const DOM = document.getElementById('app');
+        const HTML = this.header() + this.main() + this.footer();
+
+        DOM.insertAdjacentHTML("beforeend", HTML);
+    }
+
+}
---
+++ js/pages/home.js
@@ -0,0 +1,7 @@
+import { layout } from "../components/layout.js";
+
+const homePage = new layout();
+homePage.render();
+
+
+
